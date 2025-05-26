072720d851a1cc8e9f02b74c6a6bdfc6d35b36a3
Muhammed Akintola <akintola573@gmail.com>
2025-05-26 11:43:31 +0100

    edited Jenkins

--- Jenkins/child-pipeline.groovy
+++ Jenkins/child-pipeline.groovy
@@ -9,7 +9,7 @@ pipeline {
         stage('Load Context') {
             steps {
                 script {
-                    def context = [:]
+                    context = [:]
                     def contextFile = "/var/jenkins_home/workspace/parent-pipeline-job/context_env"
                     if (!fileExists(contextFile)) {
                         error "context_env not found: $contextFile"
@@ -22,21 +22,17 @@ pipeline {
                     }
 
                     echo "Loaded context: GIT_URL=${context['GIT_URL']}, GIT_BRANCH=${context['GIT_BRANCH']}"
-
-                    // Store in script-level vars
-                    env.GIT_URL = context['GIT_URL']
-                    env.GIT_BRANCH = context['GIT_BRANCH']
-                    env.PROJECT_NAME = context['GIT_URL'].tokenize("/")[4].replace(".git", "")
-                    env.BRANCH_NAME = context['GIT_BRANCH'].tokenize("/")[1]
                 }
             }
         }
 
         stage('Checkout') {
             steps {
-                git url: "${env.GIT_URL}",
-                    branch: "${env.BRANCH_NAME}",
-                    credentialsId: 'github-credentials'
+                script {
+                    git url: "${context['GIT_URL']}",
+                        branch: "${context['GIT_BRANCH'].split('/')[1]}",
+                        credentialsId: 'github-credentials'
+                }
             }
         }
 
