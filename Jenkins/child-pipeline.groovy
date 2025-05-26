8da3c8c1b5b857d541c66b6e5ca10cb33f250730
Muhammed Akintola <akintola573@gmail.com>
2025-05-26 11:40:12 +0100

    edited Jenkins

--- Jenkins/child-pipeline.groovy
+++ Jenkins/child-pipeline.groovy
@@ -2,13 +2,14 @@ pipeline {
     agent any
 
     environment {
-        DOCKER_REPO = 'amor573/tojuresto'  // Your Docker Hub repo
+        DOCKER_REPO = 'amor573/tojuresto'
     }
 
     stages {
         stage('Load Context') {
             steps {
                 script {
+                    def context = [:]
                     def contextFile = "/var/jenkins_home/workspace/parent-pipeline-job/context_env"
                     if (!fileExists(contextFile)) {
                         error "context_env not found: $contextFile"
@@ -17,11 +18,16 @@ pipeline {
                     def lines = readFile(contextFile).split("\n")
                     lines.each { line ->
                         def (key, value) = line.tokenize("=")
-                        env."${key}" = value
+                        context[key.trim()] = value.trim()
                     }
 
-                    env["PROJECT_NAME"] = env["GIT_URL"].tokenize("/")[4].replace(".git", "")
-                    env["BRANCH_NAME"] = env["GIT_BRANCH"].tokenize("/")[1]
+                    echo "Loaded context: GIT_URL=${context['GIT_URL']}, GIT_BRANCH=${context['GIT_BRANCH']}"
+
+                    // Store in script-level vars
+                    env.GIT_URL = context['GIT_URL']
+                    env.GIT_BRANCH = context['GIT_BRANCH']
+                    env.PROJECT_NAME = context['GIT_URL'].tokenize("/")[4].replace(".git", "")
+                    env.BRANCH_NAME = context['GIT_BRANCH'].tokenize("/")[1]
                 }
             }
         }
@@ -54,7 +60,5 @@ pipeline {
                 }
             }
         }
-
-        
     }
 }
